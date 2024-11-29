ec1cc184cadb1ed45391be9f45973ceac98aa28e
sayeed-hamoomi <sayeedhamoommi653@gmail.com>
2024-11-29 21:02:01 +0530

    added middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI,APIRouter
-from app.routers import users,auth,friends,expenses,transaction,dashboard
+from fastapi import FastAPI, APIRouter, Request
+from app.routers import users, auth, friends, expenses, transaction, dashboard
+import time
+from datetime import datetime
 
-app=FastAPI()
+app = FastAPI()
+
+
+@app.middleware("http")
+async def add_process_time(request: Request, call_next):
+
+    d = {
+        "intiationtime": datetime.now(),
+        "method": request.method,
+        "url": request.url,
+        "client_detail": request.client,
+    }
+
+    response = await call_next(request)
+    print(response)
+    d["status_code"] = response.status_code
+    d["responce_timestamp"] = datetime.now()
+    d["duration"] = d["responce_timestamp"] - d["intiationtime"]
+
+    print(d)
+
+    return response
+
 
 app.include_router(users.router)
 app.include_router(auth.router)
