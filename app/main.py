cab989dcbe3f151ca4e90deeaddd342eb419cf67
sayeed-hamoomi <sayeedhamoommi653@gmail.com>
2024-11-30 15:12:21 +0530

    completed middleware

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, APIRouter, Request
 from app.routers import users, auth, friends, expenses, transaction, dashboard
 import time
 from datetime import datetime
+import json
+
 
 app = FastAPI()
 
@@ -12,17 +14,22 @@ async def add_process_time(request: Request, call_next):
     d = {
         "intiationtime": datetime.now(),
         "method": request.method,
-        "url": request.url,
-        "client_detail": request.client,
+        "url": request.url.path,
+        "client_detail": request.headers.get("user-agent"),
     }
 
     response = await call_next(request)
-    print(response)
+
     d["status_code"] = response.status_code
     d["responce_timestamp"] = datetime.now()
     d["duration"] = d["responce_timestamp"] - d["intiationtime"]
+    d["intiationtime"] = d["intiationtime"].isoformat()
+    d["responce_timestamp"] = d["responce_timestamp"].isoformat()
+    d["duration"] = d["duration"].total_seconds()
 
-    print(d)
+    with open("logs.json", "a", encoding="utf-8") as file:
+        new_log = json.dumps(d)
+        file.write(new_log + ",\n")
 
     return response
 
